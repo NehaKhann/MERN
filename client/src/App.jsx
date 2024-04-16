4dcc12d1c84ab4de87e51db2b0a1729b0bea83e1
NehaKhann <n.nehakhan333@gmail.com>
2024-04-16 20:57:02 +0500

    Routing added and navbar comp added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,28 @@
-const App = ()=>{
-  return <h1>Our Main Root Page</h1>
-}
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Register } from "./pages/Register";
+import { Contact } from "./pages/Contact";
+import { Service } from "./pages/Service";
+import { Login } from "./pages/Login";
+import { Navbar } from "./components/Navbar";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/service" element={<Service />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
 export default App;
